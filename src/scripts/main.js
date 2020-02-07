dc2efb5d389feeaab6b87a2c1f97bc82d4386780
Senya Volkov <formobileprima@gmail.com>
2020-02-07 14:21:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('hello mate academy!');
+const cars = document.querySelector('#cars');
+const tr = [...document.querySelectorAll('tr')];
+const iterebelTr = tr.slice(1);
+
+const tHeadTR = document.querySelector('tr');
+const tHeadTHChildren = [...tHeadTR.children];
+
+function sortFunc(e) {
+  if (e.target.tagName === 'TH') {
+    const index = tHeadTHChildren.findIndex(elem => elem === e.target);
+
+    const iterebelTrValues = iterebelTr.map(item => {
+      return item.children[index].innerHTML;
+    });
+
+    if (e.target.getAttribute('data-type') === 'string') {
+      iterebelTrValues.sort();
+    } else {
+      iterebelTrValues.sort((a, b) => {
+        return a - b;
+      });
+    }
+
+    iterebelTrValues.forEach((item, i) => {
+      iterebelTr[i].children[index].innerHTML = item;
+    });
+  }
+}
+
+cars.addEventListener('click', sortFunc);
